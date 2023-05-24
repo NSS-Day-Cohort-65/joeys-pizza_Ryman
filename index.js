0bc603b0fe7b3c895361b02f3363115da77a547f
Caroline Madison <carolinemariemadison@gmail.com>
2023-05-24 12:53:41 -0500

    add main javascript file

---
+++ index.js
@@ -0,0 +1,22 @@
+/* 
+  The owners of Joey's Pizza want your team to develop some software
+  that allows employees to build customer pizzas with 2 toppings, at the
+  right size, get it paid for, bake it, and then output a message that
+  it is being delivered.
+
+  The delivery message should only happen if the pizza has been paid for.
+
+
+
+
+  Goals of the pizza project:
+  ---------------------------
+    1. Function for adding the toppings to the pizza
+    2. Function that bakes a pizza
+    3. Function that adds `size` property to pizza
+    4. Function to mark the pizza as being paid.
+    5. Function that logs a "Your {pizza details} is being delivered".
+          If pizza is not paid for yet, display "Please pay us first"
+    6. Each function copies the input object and returns
+          modified copy
+*/
